4dd513d5e7f9db4c5d25c2c9be7310a3da28428b
its_janhavi <janhavidhok22@navgurukul.org>
2024-03-24 23:49:23 +0530

    firstpage

---
+++ app2.js
@@ -0,0 +1,78 @@
+//  1) Take two nnumber a,b and print the sum,difference,product,quotient and modulus of a,b
+// let a = 50;
+// let b = 20;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+
+// 2) Take two numbers and only print the integer part when a is divided by b.
+// let a = 10;
+// let b = 3;
+// M = (a%b);
+// console.log(M);
+
+// 3)Take two numbers a,b and print the exponential power of the first number raised by the second.
+// let a = 4;
+// let b = 2;
+// N = a**b;
+// console.log(N);
+
+//4) Take two numbers a,b and only print the decimal part of the result obtained when a is divided by b.
+// let a = 50;
+// let b = 5;
+// M = a/b;
+// console.log(M);
+
+//5) Take marks in 5 subjects A,B,C,D,E and print out the average of marks from five subject marks.
+// let a = 50;
+// let b = 60;
+// let c = 70;
+// let d = 80;
+// let e = 90;
+// total = a+b+c+d+e;
+// Avg = total/5;
+// console.log(Avg);
+
+//6) Take the cost price a and selling price b of a product, and print the profit obtained on the product.
+// let a = 30;
+// let b = 20;
+// profit = (a-b);
+// console.log(profit);
+
+//7) Take a number and print out the last digit of the number
+// let a = 105;
+// M = (a%10);
+// console.log(M);
+
+// 8. Take a 2 digit number and return the first and last digits.
+// let a = 56;
+// firstnum = Math.floor(a / 10);
+// lastnum = (a%10);
+// console.log(firstnum);
+// console.log(lastnum);
+
+// 9)Write a JavaScript program to find the largest of three numbers.
+// let a = 10;
+// let b = 20;
+// let c = 15;
+
+// if( a>=b && a>=c){
+//     console.log("largest number a",a);
+// }
+// else if( b>=a && b>=c){
+//     console.log("largest number b",b);
+// }
+// else  {
+//     console.log("largest number c",c);
+// }
+
+// 10. Write a JavaScript program to check whether a number is even or odd.
+//  let a = 15;
+//  if(a%2==0){
+//     console.log("Even");
+//  }
+//  else{
+//     console.log("Odd");
+//  }
